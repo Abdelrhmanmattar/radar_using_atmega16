795fac17380b69328467de6e22e2d6aa80b93a48
abdelrhman kamal <134321565+Abdelrhmanmattar@users.noreply.github.com>
2023-07-26 21:22:29 +0300

    Add files via upload

---
+++ WindowsFormsApp16/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace WindowsFormsApp16
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(649, 450);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(-19, 384);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(684, 66);
+            this.panel1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(553, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Font = new System.Drawing.Font("Arial", 13F);
+            this.label2.Location = new System.Drawing.Point(59, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(458, 38);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApp16/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.IO.Ports;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp16
+{
+    public partial class Form1 : Form
+    {
+        private List<Point> drawnPoints = new List<Point>();
+
+        private Point radarCenter , radarHead;
+        private int radius;
+        private double angle;
+        Bitmap bwm;
+        Graphics graphics;
+        Pen pen;
+        double distance;
+        PointF point;
+        static SerialPort serialPort;
+        
+
+        bool stop=true;
+        public Form1()
+        {
+            InitializeComponent();
+            WindowState = FormWindowState.Maximized;
+
+            serialPort = new SerialPort("COM10", 9600);
+            serialPort.DataBits = 8;
+            serialPort.StopBits = StopBits.One;
+            serialPort.Parity = Parity.None;
+            serialPort.Handshake = Handshake.None;
+            serialPort.DataReceived += SerialPortDataReceived;
+            bwm = new Bitmap(this.Width + 1, this.Height + 1);
+            this.BackColor = Color.Black;
+
+            try
+            {
+                serialPort.Open();
+                MessageBox.Show("CONNECT OPEN");
+                serialPort.Close();
+            }
+            catch (Exception E)
+            {
+                MessageBox.Show(E.Message);
+            }
+            try
+            {
+                serialPort.Open();
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            InitializeRadar();
+        }
+ 
+        private void SerialPortDataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (stop) return;
+            string data = serialPort.ReadTo("\n");
+            string[] strings = data.Split(' ');
+            int dis=-1, an=-1;
+            double R=-1;
+            string s;
+
+            try
+            {
+                an = int.Parse(strings[1]);
+                R = an * Math.PI / 180.0;
+            }
+            catch { }
+            if (strings[0] == "-1") s = "no";
+            else s = strings[0];
+
+            BeginInvoke(new Action(() =>
+            {
+                this.label2.Text = "DISTANCE = "+s + " ANGLE=" + strings[1];
+            }));
+
+
+
+            if (R <= 0.0 || R >= 3.14)
+            {
+                drawnPoints.Clear();
+                angle = R;
+                bwm = new Bitmap(this.Width + 1, this.Height + 1);
+                paint_every();
+                this.Invalidate();
+            }
+            if (strings[0]!="-1")
+            {
+                dis = Convert.ToInt32(strings[0]);
+                an = Convert.ToInt32(strings[1]);
+                point = DRAW_POINT(dis, an);
+                PaintReceivedPoint((int)point.X,(int) point.Y);
+            }
+            return;
+        }
+
+        private void PaintReceivedPoint(int x, int y)
+        {
+            try
+            {
+                drawnPoints.Add(new Point(x, y));
+                graphics = Graphics.FromImage(bwm);
+
+                pen = new Pen(Color.Yellow, 5);
+                graphics.DrawEllipse(pen, x - 4, y - 4, 6, 6);
+                graphics.Dispose();
+                pen.Dispose();
+                this.pictureBox1.Image = bwm;
+            }
+            catch { }
+        }
+
+
+
+        private void InitializeRadar()
+        {
+            radarCenter = new Point(ClientSize.Width+300 , 2*ClientSize.Height);
+            radius = Math.Min(2*ClientSize.Width - 50, 2*ClientSize.Height-50);
+            angle = 0;
+            paint_every();
+        }
+        private void paint_every()
+        {
+            try
+            {
+                if (stop)
+                {
+                    return;
+                }
+
+                CalculateRadarHead();
+                graphics = Graphics.FromImage(bwm);
+                pen = new Pen(Color.Red, 5);
+
+                // Draw the radar head
+                graphics.DrawLine(pen, radarCenter, radarHead);
+                distance = Math.Pow(radarCenter.X - radarHead.X, 2) + Math.Pow(radarCenter.Y - radarHead.Y, 2);
+                distance = Math.Sqrt(distance);
+
+
+                pen = new Pen(Color.Green, 5);
+                graphics.DrawEllipse(pen, radarCenter.X - (float)distance, radarCenter.Y - (float)distance, (float)distance * 2, (float)distance * 2);//full
+                pen = new Pen(Color.Green, 2);
+                graphics.DrawEllipse(pen, radarCenter.X - (float)distance / 2, radarCenter.Y - (float)distance / 2, (float)distance, (float)distance);//.5
+
+                graphics.DrawEllipse(pen, radarCenter.X - (float)distance / 4, radarCenter.Y - (float)distance / 4, (float)distance / 2, (float)distance / 2);//.25
+
+
+                graphics.DrawEllipse(pen, radarCenter.X - (float)distance * (float).75, radarCenter.Y - (float)distance * (float).75, (float)distance * (float)1.5, (float)distance * (float)1.5);
+                int startAngle = 0;
+                int segmentAngle = 180 / 4;
+                List<Label> labels = new List<Label>();
+                pen = new Pen(Color.Green, 5);// Divide the half circle into 4 equal segments
+                for (int i = -1; i < 4; i++)
+                {
+                    int lineAngle = startAngle + (i + 1) * segmentAngle;
+                    double lineRadians = lineAngle * Math.PI / 180;
+                    int lineX = (int)(radarCenter.X + radius * Math.Cos(lineRadians));
+                    int lineY = (int)(radarCenter.Y - radius * Math.Sin(lineRadians));
+                    graphics.DrawLine(pen, radarCenter.X, radarCenter.Y, lineX, lineY);
+                    graphics.DrawString(lineAngle.ToString(), new Font("Arial", 10), Brushes.White, lineX, lineY - 10);
+                }
+
+                this.pictureBox1.Image = bwm;
+                graphics.Dispose();
+                pen.Dispose();
+            }
+            catch { }
+        }
+
+        public PointF DRAW_POINT(float distance , float angle)
+        {
+            float lol = distance * (float)17.05;
+            double lineRadians = angle * Math.PI / 180;
+            double x = radarCenter.X + lol * Math.Cos(lineRadians);
+            double y = radarCenter.Y - lol* Math.Sin(lineRadians);
+            PointF pointn = new PointF((float)x, (float)y);
+            return pointn;
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            foreach (Point pointn in drawnPoints)
+            {
+                if (IsMouseOnDrawnPoint(e.Location, pointn))
+                {
+                    double lol = Math.Pow(radarCenter.X - pointn.X, 2) + Math.Pow(radarCenter.Y - pointn.Y, 2);
+                    double LLLL = (Math.Sqrt(lol))*40/682;
+                    double aaaa=Math.Abs(CalculateAngleWithXAxis(pointn.X, pointn.Y));
+                    // Display a message box with (distance, angle) of the point
+                    MessageBox.Show($"distance = {LLLL.ToString("F2")}\nangle = {aaaa.ToString("F2")}");
+                    return;
+                }
+            }
+        }
+
+        private double CalculateAngleWithXAxis(double x1, double y1)
+        {
+            double angleRadians = Math.Atan2(y1 - radarCenter.Y, x1 - radarCenter.X);
+            double angleDegrees = angleRadians * (180.0 / Math.PI);
+            return angleDegrees;
+        }
+        private bool IsMouseOnDrawnPoint(Point mouseLocation, Point point)
+        {
+            Rectangle pointRect = new Rectangle(point.X - 4, point.Y - 4, 6, 6);
+            return pointRect.Contains(mouseLocation);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            stop = !stop;
+            if (stop) this.button1.Text = "START";
+            else this.button1.Text = "STOP";
+            try
+            {
+                serialPort.Write("0");
+            }
+            catch (Exception s)
+            { 
+                MessageBox.Show(s.Message);
+            }
+       
+        }
+        private void CalculateRadarHead()
+        {
+            int x = (int)(radarCenter.X + radius * Math.Cos(angle));
+            int y = (int)(radarCenter.Y - radius * Math.Sin(angle));
+            radarHead = new Point(x, y);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {}
+    }
+
+}
